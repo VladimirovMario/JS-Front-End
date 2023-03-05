3b2421c91d0df8d4b08d42cf8fce989d107fb5f9
VladimirovMario <mariovladimirovfs@gmail.com>
2023-03-05 11:15:07 +0200

    components

---
+++ 02. React -2023/03. Intro to React and JSX/03. My First React Project/first-react-project/src/components/Schedule.js
@@ -0,0 +1,20 @@
+import NavTabs from "./NavTabs";
+import TabPanes from "./TabPanes";
+
+export default function Schedule() {
+  return (
+    <div className="container">
+      <div className="row me-row schedule" id="schedule">
+        <h2 className="row-title content-ct">Event Schedule</h2>
+        <div className="col-md-12">
+
+          {/* <!-- Nav tabs --> */}
+          <NavTabs />
+          {/* <!-- Tab panes --> */}
+          <TabPanes />
+          
+        </div>
+      </div>
+    </div>
+  );
+}
