91fd9a0ea49e26034d7c30a04404f7f91c5fd87e
VladimirovMario <mariovladimirovfs@gmail.com>
2023-03-14 16:36:10 +0200

    home component

--- 02. React -2023/09. Routing Workshop/client/src/App.js
+++ 02. React -2023/09. Routing Workshop/client/src/App.js
@@ -1,11 +1,17 @@
 import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
 
 function App() {
   return (
     <div id="box">
-
       <Header />
-      
+
+      <main id="main-content">
+
+      <Home />
+
+      </main>
+
     </div>
   );
 }
