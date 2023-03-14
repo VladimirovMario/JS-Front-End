f1a886c32760338d5f48c97438a17b6f2a834fe4
VladimirovMario <mariovladimirovfs@gmail.com>
2023-03-14 16:29:42 +0200

    header component

--- 02. React -2023/09. Routing Workshop/client/src/App.js
+++ 02. React -2023/09. Routing Workshop/client/src/App.js
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header/Header";
+
 function App() {
   return (
-    <div>
-      <h1>Games play</h1>
+    <div id="box">
+
+      <Header />
+      
     </div>
   );
 }
