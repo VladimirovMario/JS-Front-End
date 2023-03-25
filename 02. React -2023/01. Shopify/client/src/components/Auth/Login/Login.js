0a129c4afc507ef3111467248c848b875bad3f38
VladimirovMario <mariovladimirovfs@gmail.com>
2023-03-25 17:25:44 +0200

    made controlled form

--- 02. React -2023/01. Shopify/client/src/components/Auth/Login/Login.js
+++ 02. React -2023/01. Shopify/client/src/components/Auth/Login/Login.js
@@ -1,42 +1,68 @@
-import styles from '../Register/Register.module.css'
+import styles from "../Register/Register.module.css";
 
-export default function Login () {
-    return(
-        <section className="section">
-        <h2 className="section-title">Sign In</h2>
-        <div className="section-divider"></div>
+import { useAuthContext } from "../../../contexts/AuthContext";
+import { useForm } from "../../../hooks/useForm";
 
-        <div className={`${styles["login"]} action-container`}>           
-            <div className={styles["login-img-wrapper"]}>
-                <img className={styles["login-img"]} src="/static/images/register-login.jpg" alt="login.jpg" />
-            </div>
+export default function Login() {
+  const { onLoginSubmit } = useAuthContext();
 
-            <form className={styles["login-form"]}>
-                <h3 className={styles["form-container-title"]}>Welcome back</h3>
-                
-                {/* <!-- Inputs --> */}
-                <div className={styles["input-login"]}>
-                    <label htmlFor="email">
-                        <span>Email</span>
-                        <input className={styles["email"]} type="email" id="email" name="email" placeholder="peter@abv.bg" />
-                    </label>
+  const { values, onChangeHandler, onSubmit } = useForm(
+    {
+      email: "",
+      password: "",
+    },
+    onLoginSubmit
+  );
 
-                    <label htmlFor="password">
-                        <span>Password</span>
-                        <input className={styles["password"]} type="password" id="password" name="password"
-                            placeholder="*****" />
-                    </label>
+  return (
+    <section className="section">
+      <h2 className="section-title">Sign In</h2>
+      <div className="section-divider"></div>
 
-                    <label htmlFor="repass">
-                        <span>Repeat</span>
-                        <input className={styles["repass"]} type="password" id="repass" name="repass" placeholder="*****" />
-                    </label>
-
-                    <button className={"form-btn btn"}>Sign in</button>
-                </div>
-
-            </form>
+      <div className={`${styles["login"]} action-container`}>
+        <div className={styles["login-img-wrapper"]}>
+          <img
+            className={styles["login-img"]}
+            src="/static/images/register-login.jpg"
+            alt="login.jpg"
+          />
         </div>
+
+        <form onSubmit={onSubmit} className={styles["login-form"]}>
+          <h3 className={styles["form-container-title"]}>Welcome back</h3>
+          {/* <!-- Inputs --> */}
+          <div className={styles["input-login"]}>
+
+            <label htmlFor="email">
+              <span>Email</span>
+              <input
+                className={styles["email"]}
+                type="email"
+                id="email"
+                placeholder="peter@abv.bg"
+                name="email"
+                value={values.email}
+                onChange={onChangeHandler}
+              />
+            </label>
+
+            <label htmlFor="password">
+              <span>Password</span>
+              <input
+                className={styles["password"]}
+                type="password"
+                id="password"
+                placeholder="*****"
+                name="password"
+                value={values.password}
+                onChange={onChangeHandler}
+              />
+            </label>
+
+            <button className={"form-btn btn"}>Sign in</button>
+          </div>
+        </form>
+      </div>
     </section>
-    );
+  );
 }
