5ccd53dc4732c28384da2e550c0f38f45b694505
VladimirovMario <mariovladimirovfs@gmail.com>
2023-03-25 19:17:02 +0200

    made controlled register form

--- 02. React -2023/01. Shopify/client/src/components/Auth/Register/Register.js
+++ 02. React -2023/01. Shopify/client/src/components/Auth/Register/Register.js
@@ -1,52 +1,111 @@
-import styles from './Register.module.css'
+import { useAuthContext } from "../../../contexts/AuthContext";
+import { useForm } from "../../../hooks/useForm";
+import styles from "./Register.module.css";
 
-export default function Register () {
-    return (
+export default function Register() {
+  const { onRegisterSubmit } = useAuthContext();
+
+  const { values, onChangeHandler, onSubmit } = useForm(
+    {
+      email: "",
+      username: "",
+      tel: "",
+      password: "",
+      repass: "",
+    },
+    onRegisterSubmit
+  );
+
+  return (
     <section className="section">
-        <h2 className="section-title">Sign Up</h2>
-        <div className="section-divider"></div>
+      <h2 className="section-title">Sign Up</h2>
+      <div className="section-divider"></div>
 
-        <div className={`${styles["register"]} action-container`}>           
-            <div className={styles["register-img-wrapper"]}>
-                <img className={styles["register-img"]} src="/static/images/register-login.jpg" alt="register.jpg" />
-            </div>
-
-            <form className={styles["register-form"]}>
-                <h3 className={styles["form-container-title"]}>Create your account today</h3>
-
-                {/* <!-- Inputs --> */}
-                <div className={styles["input-register"]}>
-                    <label htmlFor="email">
-                        <span>Email</span>
-                        <input className={styles["email"]} type="email" id="email" name="email" placeholder="peter@abv.bg" />
-                    </label>
-
-                    <label htmlFor="username">
-                        <span>Username</span>
-                        <input className={styles["username"]} type="text" id="username" name="username" placeholder="Peter" />
-                    </label>
-
-                    <label htmlFor="tel">
-                        <span>Telephone</span>
-                        <input className={styles["tel"]} type="text" id="tel" name="tel" placeholder="+359885888888" />
-                    </label>
-
-                    <label htmlFor="password">
-                        <span>Password</span>
-                        <input className={styles["password"]} type="password" id="password" name="password"
-                            placeholder="*****" />
-                    </label>
-
-                    <label htmlFor="repass">
-                        <span>Repeat</span>
-                        <input className={styles["repass"]} type="password" id="repass" name="repass" placeholder="*****" />
-                    </label>
-
-                    <button className={"form-btn btn"}>Sign Up</button>
-                </div>
-
-            </form>
+      <div className={`${styles["register"]} action-container`}>
+        <div className={styles["register-img-wrapper"]}>
+          <img
+            className={styles["register-img"]}
+            src="/static/images/register-login.jpg"
+            alt="register.jpg"
+          />
         </div>
+
+        <form onSubmit={onSubmit} className={styles["register-form"]}>
+          <h3 className={styles["form-container-title"]}>
+            Create your account today
+          </h3>
+
+          {/* <!-- Inputs --> */}
+          <div className={styles["input-register"]}>
+            <label htmlFor="email">
+              <span>Email</span>
+              <input
+                className={styles["email"]}
+                type="email"
+                id="email"
+                placeholder="peter@abv.bg"
+                name="email"
+                value={values.email}
+                onChange={onChangeHandler}
+              />
+            </label>
+
+            <label htmlFor="username">
+              <span>Username</span>
+              <input
+                className={styles["username"]}
+                type="text"
+                id="username"
+                placeholder="Peter"
+                name="username"
+                value={values.username}
+                onChange={onChangeHandler}
+              />
+            </label>
+
+            <label htmlFor="tel">
+              <span>Telephone</span>
+              <input
+                className={styles["tel"]}
+                type="text"
+                id="tel"
+                placeholder="+359885888888"
+                name="tel"
+                value={values.tel}
+                onChange={onChangeHandler}
+              />
+            </label>
+
+            <label htmlFor="password">
+              <span>Password</span>
+              <input
+                className={styles["password"]}
+                type="password"
+                id="password"
+                placeholder="*****"
+                name="password"
+                value={values.password}
+                onChange={onChangeHandler}
+              />
+            </label>
+
+            <label htmlFor="repass">
+              <span>Repeat</span>
+              <input
+                className={styles["repass"]}
+                type="password"
+                id="repass"
+                placeholder="*****"
+                name="repass"
+                value={values.repass}
+                onChange={onChangeHandler}
+              />
+            </label>
+
+            <button className={"form-btn btn"}>Sign Up</button>
+          </div>
+        </form>
+      </div>
     </section>
-    );
+  );
 }
