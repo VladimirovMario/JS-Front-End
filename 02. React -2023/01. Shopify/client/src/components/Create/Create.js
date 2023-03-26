37cd56512d8f08816c60a19607748c5d40985ae0
VladimirovMario <mariovladimirovfs@gmail.com>
2023-03-26 17:09:36 +0300

    made controlled form for create using useForm hook

--- 02. React -2023/01. Shopify/client/src/components/Create/Create.js
+++ 02. React -2023/01. Shopify/client/src/components/Create/Create.js
@@ -1,59 +1,117 @@
-import styles from './Create.module.css'
+import { useGameContext } from "../../contexts/GameContext";
+import { useForm } from "../../hooks/useForm";
+import styles from "./Create.module.css";
 
-export default function Create () {
-    return (
-        <section className="section">
-        <h2 className="section-title" >Create new product</h2>
-        <div className="section-divider"></div>
+export default function Create() {
+  const { onCreateSubmit } = useGameContext();
+  const { values, onChangeHandler, onSubmit } = useForm(
+    {
+      title: "",
+      genre: "",
+      price: "",
+      imageUrl: "",
+      description: "",
+    },
+    onCreateSubmit
+  );
 
-        <div className={"action-container"}>
+  return (
+    <section className="section">
+      <h2 className="section-title">Create new product</h2>
+      <div className="section-divider"></div>
 
-            <div className={styles["edit-img-wrapper"]}>
-                <img className={styles["create-edit-img"]} src={"/static/images/create-product.jpg"} alt="create-product.jpg" />
+      <div className={"action-container"}>
+        <div className={styles["edit-img-wrapper"]}>
+          <img
+            className={styles["create-edit-img"]}
+            src={"/static/images/create-product.jpg"}
+            alt="create-product.jpg"
+          />
+        </div>
+
+        <form onSubmit={onSubmit} className={styles["form-container"]}>
+          <h3 className={styles["form-container-title"]}>Create Publication</h3>
+          <p className={styles["form-container-desc"]}>
+            Add your own masterpiece!
+          </p>
+
+          {/* <!-- Inputs --> */}
+          <div className={styles["input-wrapper"]}>
+            <div className={styles["rows-aligned"]}>
+              <label className={styles["vertical"]} htmlFor="title">
+                <span>Title</span>
+                <input
+                  className={styles["input-title"]}
+                  type="text"
+                  id="title"
+                  placeholder="Grand Theft Auto V"
+                  name="title"
+                  value={values.title}
+                  onChange={onChangeHandler}
+                />
+              </label>
+              <label htmlFor="genre">
+                <span>Genre</span>
+                <input
+                  className={styles["input-genre"]}
+                  type="text"
+                  id="genre"
+                  placeholder="Action"
+                  name="genre"
+                  value={values.genre}
+                  onChange={onChangeHandler}
+                />
+              </label>
+              <label htmlFor="price">
+                <span>Price</span>
+                <input
+                  className={styles["input-price"]}
+                  type="number"
+                  id="price"
+                  name="price"
+                  value={values.price}
+                  onChange={onChangeHandler}
+                />
+              </label>
+              <label htmlFor="imageUrl">
+                <span>Image Url</span>
+                <input
+                  className={styles["input-img-url"]}
+                  type="text"
+                  id="imageUrl"
+                  placeholder="https://"
+                  name="imageUrl"
+                  value={values.imageUrl}
+                  onChange={onChangeHandler}
+                />
+              </label>
             </div>
 
-            <form className={styles["form-container"]}>
-                <h3 className={styles["form-container-title"]}>Create Publication</h3>
-                <p className={styles["form-container-desc"]}>Add your own masterpiece!</p>
-
-                {/* <!-- Inputs --> */}
-                <div className={styles["input-wrapper"]}>
-                    <div className={styles["rows-aligned"]}>
-                        <label className={styles["vertical"]} htmlFor="title">
-                            <span>Title</span>
-                            <input className={styles["input-title"]} type="text" id="title" name="title"
-                                placeholder="Grand Theft Auto V" />
-                        </label>
-                        <label htmlFor="genre">
-                            <span>Genre</span>
-                            <input className={styles["input-genre"]} type="text" id="genre" name="genre" placeholder="Action" />
-                        </label>
-                        <label htmlFor="price">
-                            <span>Price</span>
-                            <input className={styles["input-price"]} type="number" id="price" name="price" />
-                        </label>
-                        <label htmlFor="imageUrl">
-                            <span>Image Url</span>
-                            <input className={styles["input-img-url"]} type="text" id="imageUrl" name="imageUrl"
-                                placeholder="https://" />
-                        </label>
-                    </div>
-
-                    {/* <!-- text area --> */}
-                    <div className={styles["align-center"]}>
-                        <label htmlFor="description">
-                            <span>Description</span>
-                            <textarea className={styles["description"]} name="description" cols="40" rows="4"
-                                placeholder="Grand Theft Auto (usually abbreviated GTA) is a series of games that incorporate driving and action gameplay styles."></textarea>
-                        </label>
-                        <div className={styles["align-center-action"]}>
-                            <input className={"action-bnt create-btn btn"} type="submit" value="Create" />
-                        </div>
-                    </div>
-
-                </div>
-            </form>
-        </div>
+            {/* <!-- text area --> */}
+            <div className={styles["align-center"]}>
+              <label htmlFor="description">
+                <span>Description</span>
+                <textarea
+                  className={styles["description"]}
+                  cols="40"
+                  rows="4"
+                  placeholder="Grand Theft Auto (usually abbreviated GTA) is a series of games that incorporate driving and action gameplay styles."
+                  name="description"
+                  value={values.description}
+                  onChange={onChangeHandler}
+                ></textarea>
+              </label>
+              <div className={styles["align-center-action"]}>
+                <input
+                  className={"action-bnt create-btn btn"}
+                  type="submit"
+                  value="Create"
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </section>
-    );
+  );
 }
