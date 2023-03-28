cc641a334385fe16bdc914a517dce94e22868c62
VladimirovMario <mariovladimirovfs@gmail.com>
2023-03-28 16:39:35 +0300

    added html and css for favorites games TODO reuse them form other modules

--- 02. React -2023/01. Shopify/client/src/components/Auth/Profile/Profile.js
+++ 02. React -2023/01. Shopify/client/src/components/Auth/Profile/Profile.js
@@ -10,7 +10,7 @@ export default function Profile() {
       <article className={`${styles["profile-card"]} action-container`}>
         <article className={styles["profile-card-info"]}>
           <h2>Username: Peter</h2>
-          <h3>Address: 2572 Orphan Road</h3>
+          <h3>Email: Peter@abv.bg</h3>
           {/* <!--If the user has shared publications, separate their titles with a comma and a space (, )--> */}
           <h4>Titles of shared posts by the user: </h4>
           {/* <!--If not display:--> */}
@@ -21,10 +21,40 @@ export default function Profile() {
           {/* <!--If not display:--> */}
           {/* <!--<h4>Titles of which the user is the author: Not yet.</h4>--> */}
         </article>
-        <article className={styles["profile-card-icon"]}>
-          <img src="static/images/vip-user.png" alt="vip-user.png" />
-        </article>
+
+        <div className={styles["profile-card-icon"]}>
+          <img src="/static/images/vip-user.png" alt="vip-user.png" />
+        </div>
       </article>
+
+      <div className={styles["product-wrapper"]}>
+        <ul className={styles["product-ul"]}>
+          <li>
+            <article className={styles["card"]}>
+              <div className={styles["thumbnail"]}>
+                <img
+                  className={styles["thumbnail-img"]}
+                  src="http://localhost:3000/static/images/ps4/grand-theft-auto-v.jpg"
+                  alt="Unsplash img"
+                />
+              </div>
+
+              <div className={styles["content"]}>
+                <h3 className={styles["content-title"]}>Grand Theft Auto V</h3>
+                <div className={styles["icon-wrapper"]}>
+                  <p className={styles["content-price"]}>30$</p>
+
+                  <div className={styles["icon-btn"]}>
+                    <button className={`${styles["shopping-icon"]} btn`}>
+                      <i className="fa-solid fa-cart-shopping"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
