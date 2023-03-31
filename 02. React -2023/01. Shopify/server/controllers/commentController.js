ca6519adb6e694325b22eda1bc1eee7d2e96b178
VladimirovMario <mariovladimirovfs@gmail.com>
2023-03-31 16:30:39 +0300

    added commentController

--- 02. React -2023/01. Shopify/server/controllers/commentController.js
+++ 02. React -2023/01. Shopify/server/controllers/commentController.js
@@ -4,17 +4,22 @@ const { hasUser } = require("../middlewares/guards");
 const { getAll, createComment } = require("../services/commentService");
 const { parseError } = require("../util/parser");
 
-commentController.get("/", async (req, res) => {
-  const comments = await getAll();
+commentController.get("/:gameId", async (req, res) => {
+  const comments = await getAll(req.params.gameId);
   res.status(200).json(comments);
 });
 
-commentController.post("/", hasUser(), async (req, res) => {
+commentController.post("/:gameId", hasUser(), async (req, res) => {
   try {
-    const comment = await createComment(req.params.gameId, req.user._id, req.body);
+    const comment = await createComment(
+      req.params.gameId,
+      req.user._id,
+      req.body
+    );
+
     res.status(200).json(comment);
   } catch (error) {
-    const message = parseError(err);
+    const message = parseError(error);
     res.status(400).json({ message });
   }
 });
