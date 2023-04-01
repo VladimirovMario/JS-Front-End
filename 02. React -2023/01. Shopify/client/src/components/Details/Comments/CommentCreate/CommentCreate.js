f8c978da09276643b113d9f90377d1936af4f5b8
VladimirovMario <mariovladimirovfs@gmail.com>
2023-04-01 16:41:41 +0300

    made create comment functionality with controlled react form loaded game data to visualize picture and title

--- 02. React -2023/01. Shopify/client/src/components/Details/Comments/CommentCreate/CommentCreate.js
+++ 02. React -2023/01. Shopify/client/src/components/Details/Comments/CommentCreate/CommentCreate.js
@@ -1,41 +1,81 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { getById } from "../../../../services/gameService";
+import { useGameContext } from "../../../../contexts/GameContext";
+
 import styles from "./CommentCreate.module.css";
 
 export default function CommentCreate() {
+  const { createComment } = useGameContext();
+  const { gameId } = useParams();
+  const navigate = useNavigate();
+  const [game, setGame] = useState({});
+  const [values, setValues] = useState({
+    subject: "",
+    content: "",
+  });
+
+  useEffect(() => {
+    getById(gameId)
+      .then((res) => {
+        setGame(res);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  }, [gameId]);
+
+  const onChangeHandler = (e) => {
+    setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createComment(gameId, values);
+    navigate(`/catalog/${gameId}`);
+  };
+
   return (
     <section className="section">
       <h2 className={"section-title"}>Create Comment</h2>
       <div className={"section-divider"}></div>
-
       <div className={styles["create-comment"]}>
+        <div className={styles["game-wrapper"]}>
+          <h3 className={styles["game-title"]}>{game.title}</h3>
+          <div className={styles["image-wrapper"]}>
+            <img src={game.imageUrl} alt={`${game.title}.jpg`} />
+          </div>
+        </div>
+
         <div className={styles["form-wrapper"]}>
-          <form className={styles["form"]} action="post">
-          
-              <label htmlFor="genre">
-                <span className={styles["input-title"]}>Subject</span>
-                <input
-                  className={styles["input-subject"]}
-                  type="text"
-                  id="subject"
-                  placeholder="Subject"
-                  name="subject"
-                  //   value={values.subject}
-                  //   onChange={onChangeHandler}
-                />
-              </label>
-                 
-              <label htmlFor="content">
-                <span className={styles["input-title"]}>Content</span>
-                <textarea
-                  className={styles["message"]}
-                  cols="40"
-                  rows="4"
-                  placeholder="Sure, feel free to enter your comment!"
-                  name="content"
-                  //   value={values.content}
-                  //   onChange={onChangeHandler}
-                ></textarea>
-              </label>
-         
+          <form onSubmit={onSubmit} className={styles["form"]} action="post">
+            <label htmlFor="genre">
+              <span className={styles["input-title"]}>Subject</span>
+              <input
+                className={styles["input-subject"]}
+                type="text"
+                id="subject"
+                placeholder="Subject"
+                name="subject"
+                value={values.subject}
+                onChange={onChangeHandler}
+              />
+            </label>
+
+            <label htmlFor="content">
+              <span className={styles["input-title"]}>Content</span>
+              <textarea
+                className={styles["message"]}
+                cols="40"
+                rows="4"
+                placeholder="Sure, feel free to enter your comment!"
+                name="content"
+                value={values.content}
+                onChange={onChangeHandler}
+              ></textarea>
+            </label>
+
             <button className={"action-bnt create-btn btn"}>
               Send Comment
             </button>
