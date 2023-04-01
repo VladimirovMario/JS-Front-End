20b7813175e99a49e7dee7bb511e87a4818152b6
VladimirovMario <mariovladimirovfs@gmail.com>
2023-04-01 16:06:16 +0300

    added nested author field in comment

--- 02. React -2023/01. Shopify/server/models/Comment.js
+++ 02. React -2023/01. Shopify/server/models/Comment.js
@@ -12,6 +12,10 @@ const commentSchema = new Schema(
       required: true,
       minlength: [3, "Content should be at least 3 characters long!"],
     },
+    author: {
+      email: { type: String },
+      username: { type: String },
+    },
     gameId: { type: Types.ObjectId, ref: "Game" },
     _ownerId: { type: Types.ObjectId, ref: "User" },
     games: { type: [Types.ObjectId], ref: "Game", default: [] },
@@ -22,14 +26,3 @@ const commentSchema = new Schema(
 const Comment = model("Comment", commentSchema);
 
 module.exports = Comment;
-
-/*
-{
-  _id: "",
-  subject: "Gta V"
-  content: "Great game!",
-  _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
-  gameId: "8f414b4f-ab39-4d36-bedb-2ad69da9c830",
-  _createdOn: 1614260681375,
-};
-*/
