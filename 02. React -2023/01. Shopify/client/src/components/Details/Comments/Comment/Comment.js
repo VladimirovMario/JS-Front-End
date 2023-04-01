70c15a20e93847f6d4495133026623a1defe3bff
VladimirovMario <mariovladimirovfs@gmail.com>
2023-04-01 16:31:53 +0300

    loaded author username from database

--- 02. React -2023/01. Shopify/client/src/components/Details/Comments/Comment/Comment.js
+++ 02. React -2023/01. Shopify/client/src/components/Details/Comments/Comment/Comment.js
@@ -13,13 +13,11 @@ export default function Comment({ comment }) {
               alt=""
             />
           </div>
-          <p className={styles["author-name"]}>Peter</p>
-          <p>{comment.created_at.substring(0, 10)}</p>
+          <p className={styles["author-name"]}>{comment.author?.username}</p>
+          <p>{comment.created_at.substring(0, 10)} {comment.created_at.substring(11, 19)}</p>
         </div>
         <div className={styles["publication-wrapper"]}>
-          <h3 className={styles["publication-title"]}>
-            Subject: {comment.subject}
-          </h3>
+          <h3 className={styles["publication-title"]}>{comment.subject}</h3>
           <p className={styles["publication-desc"]}>{comment.content}</p>
         </div>
       </article>
