9c162e9de32537532092b086c2d13c8a3a48a652
VladimirovMario <mariovladimirovfs@gmail.com>
2023-04-02 20:34:38 +0300

    added product validations

--- 02. React -2023/01. Shopify/client/src/utils/formValidations.js
+++ 02. React -2023/01. Shopify/client/src/utils/formValidations.js
@@ -5,6 +5,9 @@ export const formValidations = (e) => {
   const emailRegex =
     /(?<!\S)[A-Za-z]+(\.|-|_)?[A-Za-z]+@[A-Z-a-z]+\.[A-Z-a-z]+(\.[A-Z-a-z]*\.?)?\b/;
 
+  const imageRegex = /^https?:\/\/.+$/i;
+
+  // User validations
   if (target === "email" && emailRegex.test(value) === false) {
     errors.email = "must be valid address format!";
   }
@@ -19,6 +22,27 @@ export const formValidations = (e) => {
 
   if (target === "repass" && value === "") {
     errors.repass = "don't match with password";
+  }
+
+  // Product validations
+  if (target === "title" && value.length < 3) {
+    errors.title = "should be at least 3 characters";
+  }
+
+  if (target === "genre" && value.length < 3) {
+    errors.genre = "should be at least 3 characters";
+  }
+
+  if (target === "price" && Number(value) < 0.01) {
+    errors.price = "must be a positive number";
+  }
+
+  if (target === "imageUrl" && imageRegex.test(value) === false) {
+    errors.imageUrl = "should starts with http or https";
+  }
+
+  if (target === "description" && (value.length < 4 || value.length > 1400)) {
+    errors.description = "should be between 4 and 1400 characters";
   }
 
   return errors;
