732a0867b6e16cb79e92b784e2ca3bcc3f1d5c6e
VladimirovMario <mariovladimirovfs@gmail.com>
2023-04-02 17:45:26 +0300

    added errors functionality

--- 02. React -2023/01. Shopify/client/src/hooks/useForm.js
+++ 02. React -2023/01. Shopify/client/src/hooks/useForm.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { formValidations } from "../utils/formValidations";
 
 export const useForm = (initialValues, onSubmitHandler) => {
   const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState(initialValues);
 
   const onChangeHandler = (e) => {
     setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const onValidateForm = (e) => {
+    const error = formValidations(e);
+    setErrors(error);
   };
 
   const onSubmit = (e) => {
@@ -23,7 +30,9 @@ export const useForm = (initialValues, onSubmitHandler) => {
 
   return {
     values,
+    errors,
     onChangeHandler,
     onSubmit,
+    onValidateForm,
   };
 };
