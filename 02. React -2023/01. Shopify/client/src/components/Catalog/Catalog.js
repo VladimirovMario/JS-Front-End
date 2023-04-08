f2df4cb6e5289d640b24d8150951745ad64fd374
VladimirovMario <mariovladimirovfs@gmail.com>
2023-04-08 19:12:52 +0300

    added search styles

--- 02. React -2023/01. Shopify/client/src/components/Catalog/Catalog.js
+++ 02. React -2023/01. Shopify/client/src/components/Catalog/Catalog.js
@@ -1,10 +1,27 @@
-import styles from "./Catalog.module.css";
-import ProductPs4 from "../ProductPs4/ProductPs4";
+import { useState } from "react";
+
 import { useGameContext } from "../../contexts/GameContext";
 import { Loader } from "../Shared/Loader/Loader";
+import ProductPs4 from "../ProductPs4/ProductPs4";
+
+import styles from "./Catalog.module.css";
 
 export default function Catalog() {
-  const { games, loading } = useGameContext();
+  const { games, loading, searchGamesHandler } = useGameContext();
+
+  const [value, setValues] = useState({
+    search: "",
+  });
+
+  const onChangeHandler = (e) => {
+    setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // TODO set the search parameters in the url
+    searchGamesHandler(value.search);
+  };
 
   return (
     <section className="section">
@@ -14,18 +31,25 @@ export default function Catalog() {
       {/* <!-- Products --> */}
       <div className={styles["product-wrapper"]}>
         {/* <!-- SEARCH FORM --> */}
-        {/* <form className="search-form">
-            <div className="col aligned">
-                <label>
-                    <span>Terms:</span>
-                    <input className="search-input" type="text" name="search" autocomplete="off"
-                        placeholder="Start searching for..." />
-                </label>
-            </div>
-            <label>
-                <button className="action">Search</button>
+        <form onSubmit={onSubmit} className={styles["search-form"]}>
+          
+            <label className={styles["search-label"]} htmlFor="search">
+              <span className={styles["search-terms"]}>Title:</span>
+              <input
+                className={styles["search-input"]}
+                id="search"
+                type="search"
+                autoComplete="off"
+                placeholder="Start searching for..."
+                name="search"
+                value={value.search}
+                onChange={onChangeHandler}
+              />
             </label>
-        </form>   */}
+         
+            <button className={"action-bnt edit-btn btn"}>Search</button>
+         
+        </form>
 
         {loading && <Loader />}
 
